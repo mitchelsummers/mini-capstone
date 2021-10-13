14670784c5052c6d28e3a344b966b86b0f55e7cb
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-10-13 11:35:17 -0600

    adds random product method in controller w/ route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,8 @@ class ProductsController < ApplicationController
     all_products = Product.all
     render json: all_products
   end
+  def product_random
+    product_random = Product.all.sample
+    render json: product_random
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get "/products_all" => "products#products_all"
-  
+  get "/product_random" => "products#product_random"
 end
