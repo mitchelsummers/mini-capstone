7b2b6791668f873a08f2abf00c482d9f6be05d17
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-10-13 11:31:43 -0600

    adds correct model controller all with route & seeds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def products_all
+    all_products = Product.all
+    render json: all_products
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,2 @@
-class Product
-  attr_accessor :name, :price, :image_url, :description
-  def initialize(options)
-    @name = options[:name] 
-    @price = options[:price] 
-    @image_url = options[:image_url] 
-    @description = options[:description]    
-  end
+class Product < ApplicationRecord
 end
