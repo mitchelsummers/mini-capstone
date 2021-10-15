858b422190503f8be842aec1ea7ae9eeecad2a9d
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-10-15 12:36:57 -0600

    adds routes with controller methods and web requests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,15 +4,36 @@ class ProductsController < ApplicationController
     render json: all_products
   end
   def create
+    product = Product.new(
+      name: params["name"], 
+      price: params["price"], 
+      image_url: params["image_url"], 
+      description: params["description"]
+    )
+    product.save
+    render json: product.as_json
   end
   def show
-    id_value = params[:id]
-    product_id = Product.find_by(id: id_value)
-    render json:  product_id
+    id_value = params["id"]
+    product = Product.find_by(id: id_value)
+    render json: product
   end
   def update
+    id_value = params["id"]
+    product = Product.find_by(id: id_value)
+
+    product.name = params["name"] || product.name
+    product.price = params["price"] || product.price
+    product.image_url = params["image_url"] || product.image_url
+    product.description = params["description"] || product.description
+
+    product.save
+    render json: product.as_json
   end
   def destroy
+    id_value = params["id"]
+    product = Product.find_by(id: id_value)
+    product.destroy
   end
   def product_query_parameter_id
     id_value = params[:id]
