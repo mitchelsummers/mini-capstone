c9ad6616fba959295542172f401571f33b04d9ce
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-10-15 11:20:59 -0600

    adds routes and empty methods for update and destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,16 @@ class ProductsController < ApplicationController
     all_products = Product.all
     render json: all_products
   end
+  def create
+  end
   def show
     id_value = params[:id]
     product_id = Product.find_by(id: id_value)
     render json:  product_id
+  end
+  def update
+  end
+  def destroy
   end
   def product_query_parameter_id
     id_value = params[:id]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "/products" => "products#index"
+  post "/products" => "products#create"
   get "/products/:id" => "products#show"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
   get "/product_query_parameter_id" => "products#product_query_parameter_id"
-
   post "/body_params" => "products#body_params"
 end
