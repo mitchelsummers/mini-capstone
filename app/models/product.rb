1bd386ebfa8a727c836ab16636dd66bba6841336
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-10-27 10:57:53 -0600

    adds category & product_category models with belongs_to/has_many & creates instances of categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   has_many :images
   has_many :orders
+  has_many :category_products
   belongs_to :supplier
   validates :name,  presence: true, uniqueness: true, length: { minimum: 2 }
   validates :image_url,  presence: true
