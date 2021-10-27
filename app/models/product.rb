d91f2d5cd93a87750fee09cd3e491f8d7ec8c426
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-10-27 11:34:41 -0600

    adds category_product instances and methods to call each from either

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,18 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders
   has_many :category_products
+  has_many :categories, through: :category_products
   belongs_to :supplier
+
   validates :name,  presence: true, uniqueness: true, length: { minimum: 2 }
   validates :image_url,  presence: true
   validates :description, length: { maximum: 500 }
   validates :price, presence: true, numericality: { greater_than: 0 }
+  # def categories
+  #   category_products.map do |category_product|
+  #     category_product.category
+  #   end
+  # end 
   def is_discounted?
     price < 10
   end
